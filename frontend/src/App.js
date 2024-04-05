4a9b9ce9fee6537156549aaf36ce468eca0df21c
nikhil5856 <73579413+nikhil5856@users.noreply.github.com>
2024-04-04 22:15:02 -0500

    updsted routing in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,39 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/login";
+import Signup from "./pages/signup/signup";
+import Dashboard from "./pages/dashboard/dashboard";
+import QRScanner from "./pages/scanQR/scanner";
+import VerifyProduct from "./pages/verifyProduct/verifyProduct";
 
-import './App.css';
-import {Routes, Route } from 'react-router-dom';
-import Login from './pages/login/login';
-import Signup from './pages/signup/signup';
-import Dashboard from './pages/dashboard/dashboard';
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        {/* <Route path="/about" element={<About />} /> */}
         <Route path="/" element={<Login />} />
+        <Route path="/supplier" element={<Login welcomeText="Supplier" />} />
+
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard/:id/:userName" element={<Dashboard />} />
+        <Route path="/supplier/signup" element={<Signup />} />
+
+        <Route
+          path="/dashboard/:userType/:userId/:userName"
+          element={<Dashboard />}
+        />
+
+        <Route
+          path="/scan/:userId/:userName/:userType"
+          element={<QRScanner />}
+        />
+
+        <Route
+          path="/verify/:productId/:randomNumber/:customerId/:userName/:userType"
+          element={<VerifyProduct />}
+        />
       </Routes>
     </div>
   );
 }
+
 
 export default App;
--- frontend/src/pages/viewProduct/viewProduct.jsx
+++ frontend/src/pages/viewProduct/viewProduct.jsx
@@ -1,40 +1,147 @@
-import React,{useEffect,useState} from 'react';
-import './viewProduct.css'; 
-
+import React, { useEffect, useState } from 'react';
+import './viewProduct.css';
+import { Card, Col, Container, Row, Button, Form } from 'react-bootstrap';
+import PopUp from '../../components/popup/popup';
+import Loader from '../../components/loader/loader';
 import axios from "../../axios"
 
-const ViewProducts = ({ products }) => {
+const ViewProducts = ({ userId, userType, setCartNumber, setCart, cart}) => {
 
-    const [books, setBooks] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [isPopUpOpen, setIsPopUpOpen] = useState(false);
+    const [popUpText, setpopUpText] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [isBackgroundBlurred, setIsBackgroundBlurred] = useState(false);
 
-    const fetchBooks = async () => {
-        try{
-            const res = await axios.get("/product/get-products");
-            setBooks(res.data);
-            console.log(res.data)
-        }catch(error){
-            console.log(error);
+    const blurredBackgroundStyles = isBackgroundBlurred
+        ? {
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(100, 100, 100, 0.5)",
+            backdropFilter: "blur(1.8px)",
+            zIndex: 1,
+        }
+        : {};
+
+    const handleAddToCart = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const quantity = parseInt(formData.get('quantity'), 10);
+        const productId = parseInt(formData.get('productId'), 10);
+        const productName = formData.get('productName');
+        const productPrice = formData.get('productPrice');
+        const productImage = formData.get('productImage');
+
+        const existingProductIndex = cart.findIndex((p) => p.productId === productId);
+        if (existingProductIndex !== -1) {
+            const updatedCart = [...cart];
+            updatedCart[existingProductIndex] = {
+                ...updatedCart[existingProductIndex],
+                quantity: quantity
+            };
+            setCart(updatedCart);
+        } else {
+            setCart([...cart, { productId, quantity, productName, productPrice, productImage }]);
         }
     }
-    fetchBooks();
+
+    useEffect(()=>{
+        setCartNumber(cart.length);
+    },[cart])
+
+    const fetchproducts = async () => {
+        setLoading(true);
+        try {
+            let res;
+            if (userType === "customer") {
+                res = await axios.get(`/product/get-products`);
+            } else {
+                res = await axios.get(`/product/get-products/${userId}`);
+            }
+            setProducts(res.data);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+            if (error?.response?.data?.message) {
+                setpopUpText(error?.response?.data?.message);
+            }
+            else {
+                setpopUpText("Something Went Wrong")
+            }
+            setIsPopUpOpen(true);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchproducts();
+    }, []);
+
     return (
         <div className="products-container">
-        {books.length > 0 ? (
-            books.map((product, index) => (
-            <div className="product-card" key={index}>
-                <img src={product.imageUrl} alt={product.name} className="product-image" />
-                <div className="product-info">
-                <h3>{product.name}</h3>
-                <p>{product.description}</p>
-                <p>Category: {product.category}</p>
-                <p>Price: ${product.price}</p>
-                <p>Stock: {product.stock}</p>
-                </div>
-            </div>
-            ))
-        ) : (
-            <p>No products added yet.</p>
-        )}
+            {isBackgroundBlurred && <div style={blurredBackgroundStyles} />}
+            {loading && <Loader />}
+            <Container>
+                <Row>
+                    {products.length > 0 ? (
+                        products.map((product, index) => (
+                            <Col md={3}>
+                                <Card className="product-card my-3" key={index}>
+                                    <Card.Img src={product.imageUrl} alt={product.name} className="product-image" />
+                                    <Card.Body>
+                                        <Card.Title>{product.name}</Card.Title>
+                                        <Card.Text>{product.description}</Card.Text>
+                                        <Card.Text>Category: {product.category}</Card.Text>
+                                        <Row>
+                                            <Col md={6}>
+                                                <Card.Text>Stock: {product.stock}</Card.Text>
+                                            </Col>
+                                            <Col md={6}>
+                                                <Card.Text>Price: ${product.price}</Card.Text>
+                                            </Col>
+                                        </Row>
+                                        {userType === "customer" &&
+                                            <Form onSubmit={handleAddToCart} className='my-3' style={{ alignItems: 'center' }}>
+                                                <input type="hidden" name="productId" value={product.productId} />
+                                                <input type="hidden" name="productImage" value={product.imageUrl} />
+                                                <input type="hidden" name="productName" value={product.name} />
+                                                <input type="hidden" name="productPrice" value={product.price} />
+                                                <Row className="mb-3">
+                                                    <Form.Group as={Col} md={6}>
+                                                        <Form.Control
+                                                            type="number"
+                                                            min={0}
+                                                            max={product.stock}
+                                                            placeholder="0"
+                                                            name="quantity"
+                                                            required
+                                                        />
+                                                    </Form.Group>
+
+                                                    <Form.Group as={Col} md={6}>
+                                                        <Button type="submit" className='cart-button'>Add to cart</Button>
+                                                    </Form.Group>
+                                                </Row>
+                                            </Form>
+                                        }
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))
+                    ) : (
+                        <p>No products added yet.</p>
+                    )}
+                </Row>
+            </Container>
+            <PopUp
+                isOpen={isPopUpOpen}
+                close={() => setIsPopUpOpen(false)}
+                text={popUpText}
+            />
         </div>
     );
 };
