92741c3446ef8e5d0ef0cd4d6732688b496c5de6
KAVURUVEENACHOWDARY <veenachowdarykavuri@gmail.com>
2024-04-23 12:26:56 -0500

    scheduled a job to send warranty alerst

--- backend/cronJobs/cron.js
+++ backend/cronJobs/cron.js
@@ -31,3 +31,31 @@ cron.schedule('*/10 * * * *', async () => {
     console.error('Error processing products:', error);
   }
 });
+
+// 2) Cron job to send warranty alerts (runs every 10 minutes)
+cron.schedule('*/10 * * * *', async () => {
+  const currentDate = new Date().toUTCString();
+  try {
+    const products = await Product.find({});
+    for (const product of products) {
+      if (product.expiry !== null) {
+        for (const p of product.products) {
+          if (p.customerId !== null && p.expiryDate !== null && !p.warrantyNotification) {
+            if (new Date(p.expiryDate) < new Date(currentDate)) {
+              const customer = await Customer.findById(p.customerId);
+              sendEmail(
+                customer.name, 
+                customer.email, 
+                'Warranty Alert', 
+                `Your product warranty for ${product.name} purchased on ${p.purchaseDate.toLocaleDateString()} has expired on ${p.warrantyDate.toLocaleDateString()}.`);
+                p.warrantyNotification = true;
+            }
+          }
+        }
+      }
+      await product.save();
+    }
+  } catch (error) {
+    console.error('Error processing products:', error);
+  }
+});
