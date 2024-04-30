dfecbad6c42ccb400539565428ecf14dd0aa0090
KAVURUVEENACHOWDARY <74002021+KAVURUVEENACHOWDARY@users.noreply.github.com>
2024-04-30 02:21:20 +0000

    added deployent changes

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const publicIP = '<public-ip>' // elastic IP of development server
 
-const BASE_URL = "http://localhost:3002" 
+const BASE_URL = `http://${publicIP}:3002`
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
